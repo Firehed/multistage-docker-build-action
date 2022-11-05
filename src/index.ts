77467f2d1bef531c3e744ff0a79cb67fbd7517e8
Eric Stern <eric@ericstern.com>
2022-11-04 23:41:54 -0700

    Update dependencies (#29)
    Fixes #28

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,9 @@ async function run(): Promise<void> {
     )
     await time('Full Build', build)
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
 
