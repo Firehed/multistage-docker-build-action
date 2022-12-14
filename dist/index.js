Firehed/multistage-docker-build-action

commit 77467f2d1bef531c3e744ff0a79cb67fbd7517e8

Update dependencies (#29)
Fixes #28

src/index.ts
@@ -19,7 +19,9 @@ async function run(): Promise<void> {
     )
     await time('Full Build', build)
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
 

action.yml
@@ -51,5 +51,5 @@ branding:
   icon: package
 
 runs:
-  using: node12
+  using: node16
   main: dist/index.js

commit 69ae61a900ea45b43e5ac41b3e20c04c111ccbe0

Ensure action fails when internal steps do (#35)
Fixes #34, which is apparently a regression introduced in #29
(https://github.com/Firehed/multistage-docker-build-action/pull/35/commits/20c0bcdf75ca97975bb15b2278e50ebaeee66670).

This also adds `eslint` which would likely have caught the issue
earlier.

.github/workflows/lint.yml
@@ -1,0 +1,16 @@
+name: Lint
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+
+jobs:
+  eslint:
+    name: ESLint
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - run: yarn install
+      - run: yarn lint

.github/workflows/self-test-multistage-docker-build.yml
@@ -174,3 +174,52 @@ jobs:
 
       - name: Run image
         run: docker run --rm ${{ steps.build.outputs.server-tag }}
+
+  expect-error-build-failure:
+    name: Action should fail if docker build fails
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+
+      - name: Auth to GH registry
+        uses: docker/login-action@v2
+        with:
+          registry: ghcr.io
+          username: ${{ github.repository_owner }}
+          password: ${{ secrets.GITHUB_TOKEN }}
+
+      - uses: ./
+        name: Build with bad settings
+        continue-on-error: true
+        id: build
+        with:
+          # No context/dockerfile
+          server-stage: server
+          repository: ghcr.io/firehed/actions
+
+      - name: expect failure
+        if: steps.build.outcome == 'success'
+        run: exit 1
+
+
+  expect-error-push-failure:
+    name: Action should fail if docker push fails
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+
+      # Do not log in to docker registry. This will cause the push to fail
+
+      - uses: ./
+        name: Build without login
+        continue-on-error: true
+        id: build
+        with:
+          context: examples
+          repository: ghcr.io/firehed/actions
+          stages: env, configured
+          server-stage: server
+
+      - name: expect failure
+        if: steps.build.outcome == 'success'
+        run: exit 1

dist/index.js
@@ -10834,7 +10834,11 @@ function getFullCommitHash() {
     // For many situations, that results in very confusing mismatches, especially
     // when trying to use commit hashes for build targets
     if (pullRequestEvents.includes(github.context.eventName)) {
+        // type limitation in GH context
+        // eslint-disable-next-line
         const prEvent = github.context.payload.pull_request;
+        // same
+        // eslint-disable-next-line
         return prEvent.head.sha;
     }
     return github.context.sha;
@@ -10883,7 +10887,7 @@ exports.getTaggedImageForStage = getTaggedImageForStage;
  * not throw on a nonzero exit code.
  */
 async function runDockerCommand(command, ...args) {
-    let rest = [command];
+    const rest = [command];
     if (core.getBooleanInput('quiet') && command !== 'tag') {
         rest.push('--quiet');
     }
@@ -10980,6 +10984,12 @@ async function run() {
         if (error instanceof Error) {
             core.setFailed(error.message);
         }
+        else if (typeof error === 'string') {
+            core.setFailed(error);
+        }
+        else {
+            core.setFailed('Unknown error! type: ' + typeof error);
+        }
     }
 }
 /**
@@ -11054,7 +11064,7 @@ async function buildStage(stage, extraTags) {
         // '--build-arg', 'BUILDKIT_INLINE_CACHE="1"',
         ...buildArgs, ...cacheFromArg, ...dockerfileArg, '--tag', targetTag, '--target', stage, core.getInput('context'));
         if (result.exitCode > 0) {
-            throw 'Docker build failed';
+            throw new Error('Docker build failed');
         }
         await dockerPush(targetTag);
         for (const extraTag of extraTags) {
@@ -11068,7 +11078,7 @@ async function dockerPush(taggedImage) {
     core.debug(`Pushing ${taggedImage}`);
     const pushResult = await (0, helpers_1.runDockerCommand)('push', taggedImage);
     if (pushResult.exitCode > 0) {
-        throw 'Docker push failed';
+        throw new Error('Docker push failed');
     }
 }
 /**
@@ -11078,7 +11088,7 @@ async function addTagAndPush(image, stage, tag) {
     const name = (0, helpers_1.getTaggedImageForStage)(stage, tag);
     const tagResult = await (0, helpers_1.runDockerCommand)('tag', image, name);
     if (tagResult.exitCode > 0) {
-        throw 'Docker tag failed';
+        throw new Error('Docker tag failed');
     }
     await dockerPush(name);
     return name;
@@ -11088,7 +11098,7 @@ function getAllPossibleCacheTargets() {
     const stages = (0, helpers_1.getAllStages)();
     return stages.flatMap((stage) => tags.map((tag) => (0, helpers_1.getTaggedImageForStage)(stage, tag)));
 }
-run();
+run(); // eslint-disable-line
 
 
 /***/ }),

src/helpers.ts
@@ -5,13 +5,13 @@ import * as github from '@actions/github'
 // Returns a string like "refs_pull_1_merge-bk1"
 export function getTagForRun(): string {
   const usingBuildkit = process.env.DOCKER_BUILDKIT === '1'
-  const tagFriendlyRef = process.env.GITHUB_REF?.replace(/\//g, '_')
+  const tagFriendlyRef = process.env.GITHUB_REF?.replace(/\//g, '_') as unknown as string
 
   return `${tagFriendlyRef}-bk${usingBuildkit ? '1' : '0'}`
 }
 
 export function isDefaultBranch(): boolean {
-  const defaultBranch = github.context.payload.repository?.default_branch
+  const defaultBranch = github.context.payload.repository?.default_branch as string
   return github.context.payload.ref === `refs/heads/${defaultBranch}`
 }
 
@@ -30,7 +30,11 @@ export function getFullCommitHash(): string {
   // when trying to use commit hashes for build targets
 
   if (pullRequestEvents.includes(github.context.eventName)) {
+    // type limitation in GH context
+    // eslint-disable-next-line
     const prEvent = github.context.payload.pull_request as unknown as any
+    // same
+    // eslint-disable-next-line
     return prEvent.head.sha
   }
   return github.context.sha
@@ -89,7 +93,7 @@ interface ExecResult {
  * not throw on a nonzero exit code.
  */
 export async function runDockerCommand(command: DockerCommand, ...args: string[]): Promise<ExecResult> {
-  let rest: string[] = [command]
+  const rest: string[] = [command]
   if (core.getBooleanInput('quiet') && command !== 'tag') {
     rest.push('--quiet')
   }

src/index.ts
@@ -21,6 +21,10 @@ async function run(): Promise<void> {
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
+    } else if (typeof error === 'string') {
+      core.setFailed(error)
+    } else {
+      core.setFailed('Unknown error! type: ' + typeof error)
     }
   }
 }
@@ -115,7 +119,7 @@ async function buildStage(stage: string, extraTags: string[]): Promise<string> {
       core.getInput('context'),
     )
     if (result.exitCode > 0) {
-      throw 'Docker build failed'
+      throw new Error('Docker build failed')
     }
     await dockerPush(targetTag)
 
@@ -134,7 +138,7 @@ async function dockerPush(taggedImage: string): Promise<void> {
     taggedImage,
   )
   if (pushResult.exitCode > 0) {
-    throw 'Docker push failed'
+    throw new Error('Docker push failed')
   }
 }
 
@@ -145,7 +149,7 @@ async function addTagAndPush(image: string, stage: string, tag: string): Promise
   const name = getTaggedImageForStage(stage, tag)
   const tagResult = await runDockerCommand('tag', image, name)
   if (tagResult.exitCode > 0) {
-    throw 'Docker tag failed'
+    throw new Error('Docker tag failed')
   }
   await dockerPush(name)
   return name
@@ -158,4 +162,4 @@ function getAllPossibleCacheTargets(): string[] {
   return stages.flatMap((stage) => tags.map((tag) => getTaggedImageForStage(stage, tag)))
 }
 
-run()
+run() // eslint-disable-line